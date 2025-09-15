4fcd9210941c3356d2ca7c90c9bd6522ffecf5ae
Aleiaco02 <alessandroiacovelli83@gmail.com>
2025-09-15 17:00:14 +0200

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 // dichiaro il bottone che serve per generare le mail
-const bottone = document.querySelector(".btn");
+const bottone = document.querySelector(".container-bottone");
+const container = document.querySelector(".container");
+const titolo = document.querySelector(".titolo");
 
 // dichiaro l'endpoint
 const endPointApi = `https://flynn.boolean.careers/exercises/api/random/mail`
@@ -9,27 +11,31 @@ const endPointApi = `https://flynn.boolean.careers/exercises/api/random/mail`
 
 bottone.addEventListener("click", mailRandom);
 
-function mailRandom(number) {
-    axios.get(endPointApi)
-        .then(rispApi => {
-            let result = rispApi.data.response;
-            let contatore =0;
-            for (let i = 0; i < 9; i++) {
-                if (contatore < 10) {
+function mailRandom() {
+    for (let i = 0; i < 10; i++) {
+
+        axios.get(endPointApi)
+            .then(rispApi => {
+                let result = rispApi.data.response;
                 
-            }
-            }
-            
+                container.innerHTML += `
+                <div class="row text-center">
+                    <ul class="col-12">
+                        <li>${result}</li>
+                    </ul>
+                </div>`
+                // bottone.classList.add("d-none");
+                // titolo.classList.add("d-none");
 
-            console.log(result);
 
-
-
-        })
-        .catch(error => {
-            // codice da eseguire in caso di errore
-            console.error(error)
-        })
+            })
+            .catch(error => {
+                // codice da eseguire in caso di errore
+                console.error(error)
+            })
+    }
 }
+
+mailRandom();
 
 
